25497b42742d54ad5a31ac226135636107d9ebca
Marko Urbanc <urbancmarko1@gmail.com>
2024-07-25 16:22:20 +0200

    feat: :art: work on gridscan plot

---
+++ MolecularChain2/Code/T-aR-grid-plot.py
@@ -0,0 +1,91 @@
+"""
+Plot results of Temperature vs. Annealing Rate for fixed initial state
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import h5py
+import cmasher as cmr
+import palettable as pl
+
+# Enable custom stylesheet
+plt.style.use('./ma-style.mplstyle')
+mpl.use("qtagg")
+
+# Simulation parameters
+save_path = "./MolecularChain2/Results/par-gridscan-T-annealR.h5"
+bounds = (-18, 0)
+length = 17
+temperature = 1
+init = [0, -17, -4, -2, -8, -13, -7, -2, -5, -2, -3, -2, -3, -10, -15, -7, 0]
+molecules = np.arange(0, 17)
+T_values = np.loadtxt("./MolecularChain2/Code/T_range.lst")
+anneal_values = np.loadtxt("./MolecularChain2/Code/aR_range.lst")
+
+# Containers for data
+steps_to_convergence = []
+end_energy = []
+# Load data
+with h5py.File(save_path, "r") as f:
+    num_runs = len(f.keys())
+    runs = np.arange(0, num_runs)
+
+    for T in T_values:
+        row_steps = []
+        row_end_energy = []
+        for aR in anneal_values:
+            key = f"{T}-{aR}"
+            group = f[key]
+            row_steps.append(len(group[f"energy-{key}"]))
+            row_end_energy.append(group[f"energy-{key}"][-1])
+
+        steps_to_convergence.append(row_steps)  
+        end_energy.append(row_end_energy)
+
+# Calculate statistics
+steps_to_convergence = np.array(steps_to_convergence)
+print(steps_to_convergence.shape)
+avg_steps = np.mean(steps_to_convergence)
+delta_steps = np.abs(steps_to_convergence - avg_steps)
+
+end_energy = np.array(end_energy)
+print(end_energy.shape)
+
+# Plot data as 2 heatmaps
+colors = ["#37123c","#d72483","#ddc4dd","#60afff","#98CE00"]
+cm = pl.scientific.sequential.Batlow_7.mpl_colormap
+cm = cmr.get_sub_cmap(cm, 0.15, 0.85)
+
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+
+norm = mpl.colors.PowerNorm(gamma=0.2, vmin=delta_steps.min(), vmax=delta_steps.max())
+sm = plt.cm.ScalarMappable(cmap=cm, norm=norm)
+
+ax[0].imshow(delta_steps, cmap=cm, aspect="auto", norm=norm,
+             extent=[anneal_values[0], anneal_values[-1], T_values[-1], T_values[0]])
+cbar = plt.colorbar(sm, ax=ax[0])
+cbar.set_ticks(np.logspace(np.log10(delta_steps.min()), np.log10(delta_steps.max()), 10))
+avg_steps_text = f"Avg. Steps: {avg_steps:.2f}\nCMap scaling: $y=x^{{{0.2}}}$"
+ax[0].text(0.05, 0.9, avg_steps_text, transform=ax[0].transAxes,
+           fontsize=12, verticalalignment='center', horizontalalignment='left',
+           color='white', bbox=dict(facecolor='black', alpha=0.2, edgecolor='black', boxstyle='round,pad=0.5'))
+ax[0].set_xlabel("Annealing Rate")
+ax[0].set_ylabel("Temperature")
+ax[0].set_title("Deviation from Avg. Steps to Convergence")
+
+
+norm = mpl.colors.Normalize(vmin=end_energy.min(), vmax=end_energy.max())
+sm = plt.cm.ScalarMappable(cmap=cm, norm=norm)
+
+ax[1].imshow(end_energy, cmap=cm, aspect="auto",
+                extent=[anneal_values[0], anneal_values[-1], T_values[-1], T_values[0]])
+
+ax[1].set_xlabel("Annealing Rate")
+ax[1].set_ylabel("Temperature")
+ax[1].set_title("Final Energy")
+plt.colorbar(sm, ax=ax[1])
+
+plt.savefig("./MolecularChain2/Images/T-aR-gridscan.png", dpi=400)
+plt.tight_layout()
+plt.show()
