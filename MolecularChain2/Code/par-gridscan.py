ac1c6a0e18ed42a541969de70b44baabc035f330
Marko Urbanc <urbancmarko1@gmail.com>
2024-07-25 23:27:09 +0200

    feat: :package: add second parameter gridscan

--- MolecularChain2/Code/par-gridscan.py
+++ MolecularChain2/Code/par-gridscan.py
@@ -15,27 +15,27 @@ from sys import argv
 
 bounds = (-18, 0)
 length = 17
-temperature = float(argv[1])
+temperature = 1
 init = [0, -17, -4, -2, -8, -13, -7, -2, -5, -2, -3, -2, -3, -10, -15, -7, 0]
 molecules = np.arange(0, 17)
 
 m = Metropolis1(length, temperature, bounds, states=init)
 m.EXIT_COND2 = False
 m.STOP_STEPS = 50
-m.EPS = 1e-14
-m.ANNEAL_RATE = float(argv[2])
+m.EPS = float(argv[1])
+m.DELTA = float(argv[2])
 s_init, s_final, en = m.run()
 
 if True:
-    save_path = "./MolecularChain2/Results/par-gridscan-T-annealR.h5"
-    T_number = argv[1]
-    anneal_number = argv[2]
-    print(f"Storing run {T_number, anneal_number} to '{save_path}'..")
+    save_path = "./MolecularChain2/Results/par-gridscan-EPS-DELTA.h5"
+    arg1 = argv[1]
+    arg2 = argv[2]
+    print(f"Storing run {arg1, arg2} to '{save_path}'..")
 
     with h5py.File(save_path, "a") as f:
-        group = f.create_group(f"{T_number}-{anneal_number}")
-        dset_state = group.create_dataset(f"state-{T_number}-{anneal_number}", data=s_final)
-        dset_en = group.create_dataset(f"energy-{T_number}-{anneal_number}", data=en)
-        dset_t = group.create_dataset(f"temperature-{T_number}-{anneal_number}", data=m.temperatures)
+        group = f.create_group(f"{arg1}-{arg2}")
+        dset_state = group.create_dataset(f"state-{arg1}-{arg2}", data=s_final)
+        dset_en = group.create_dataset(f"energy-{arg1}-{arg2}", data=en)
+        dset_t = group.create_dataset(f"temperature-{arg1}-{arg2}", data=m.temperatures)
 
     print(f"Saved!")
--- MolecularChain2/Code/par-lst-gen.py
+++ MolecularChain2/Code/par-lst-gen.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-# sequence = np.logspace(np.log10(0.1), np.log10(100), num=100, endpoint=True)
-sequence = np.linspace(0.8, 0.9999, num=100, endpoint=True)
+# sequence = np.logspace(np.log10(1e-14), np.log10(1e-4), num=10, endpoint=True)
+# sequence = np.linspace(1e-14, 1e-4, num=100, endpoint=True)
+sequence = np.arange(1, 11, 1)
 
-with open('/home/pengu5055/Documents/FMF1mag/mod108/MolecularChain2/Code/aR_range.lst', 'w') as file:
+with open('/home/pengu5055/Documents/FMF1mag/mod108/MolecularChain2/Code/DELTA_range.lst', 'w') as file:
     for number in sequence:
         file.write(f'{number}\n')
