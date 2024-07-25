c630e07ac3b4b0f1809e5c9fec5e4a01269b9919
Marko Urbanc <urbancmarko1@gmail.com>
2024-07-25 16:44:37 +0200

    feat: :art: add first gridscan plot

--- MolecularChain2/Code/T-aR-grid-plot.py
+++ MolecularChain2/Code/T-aR-grid-plot.py
@@ -44,12 +44,12 @@ with h5py.File(save_path, "r") as f:
 
 # Calculate statistics
 steps_to_convergence = np.array(steps_to_convergence)
-print(steps_to_convergence.shape)
 avg_steps = np.mean(steps_to_convergence)
 delta_steps = np.abs(steps_to_convergence - avg_steps)
 
 end_energy = np.array(end_energy)
-print(end_energy.shape)
+avg_energy = np.mean(end_energy)
+delta_energy = end_energy - avg_energy
 
 # Plot data as 2 heatmaps
 colors = ["#37123c","#d72483","#ddc4dd","#60afff","#98CE00"]
@@ -67,24 +67,33 @@ ax[0].imshow(delta_steps, cmap=cm, aspect="auto", norm=norm,
 cbar = plt.colorbar(sm, ax=ax[0])
 cbar.set_ticks(np.logspace(np.log10(delta_steps.min()), np.log10(delta_steps.max()), 10))
 avg_steps_text = f"Avg. Steps: {avg_steps:.2f}\nCMap scaling: $y=x^{{{0.2}}}$"
-ax[0].text(0.05, 0.9, avg_steps_text, transform=ax[0].transAxes,
-           fontsize=12, verticalalignment='center', horizontalalignment='left',
+ax[0].text(0.05, 0.95, avg_steps_text, transform=ax[0].transAxes,
+           fontsize=12, verticalalignment='top', horizontalalignment='left',
            color='white', bbox=dict(facecolor='black', alpha=0.2, edgecolor='black', boxstyle='round,pad=0.5'))
 ax[0].set_xlabel("Annealing Rate")
 ax[0].set_ylabel("Temperature")
 ax[0].set_title("Deviation from Avg. Steps to Convergence")
 
 
-norm = mpl.colors.Normalize(vmin=end_energy.min(), vmax=end_energy.max())
+# Plot 2
+cm = pl.scientific.diverging.Berlin_3.mpl_colormap
+cm = cmr.get_sub_cmap(cm, 0.15, 0.85, N=len(range(int(delta_energy.min()), int(delta_energy.max()))))
+norm = mpl.colors.TwoSlopeNorm(vmin=delta_energy.min(), vmax=delta_energy.max(), vcenter=0)
 sm = plt.cm.ScalarMappable(cmap=cm, norm=norm)
 
-ax[1].imshow(end_energy, cmap=cm, aspect="auto",
+ax[1].imshow(delta_energy, cmap=cm, aspect="auto", norm=norm,
                 extent=[anneal_values[0], anneal_values[-1], T_values[-1], T_values[0]])
+cbar = plt.colorbar(sm, ax=ax[1])
+manual_ticks = np.array([delta_energy.min(), -20, -10, 0, 100, 500, 750, delta_energy.max()])
+cbar.set_ticks(manual_ticks)
+avg_steps_text = f"Avg. End Energy: {avg_energy:.2f}$"
+ax[1].text(0.05, 0.95, avg_steps_text, transform=ax[1].transAxes,
+           fontsize=12, verticalalignment='top', horizontalalignment='left',
+           color='white', bbox=dict(facecolor='black', alpha=0.2, edgecolor='black', boxstyle='round,pad=0.5'))
 
 ax[1].set_xlabel("Annealing Rate")
 ax[1].set_ylabel("Temperature")
-ax[1].set_title("Final Energy")
-plt.colorbar(sm, ax=ax[1])
+ax[1].set_title("Deviation from Avg. Final Energy")
 
 plt.savefig("./MolecularChain2/Images/T-aR-gridscan.png", dpi=400)
 plt.tight_layout()
