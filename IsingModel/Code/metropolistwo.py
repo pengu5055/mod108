1821bbf0da272800a93d8ef4dac8306ad3efdade
Marko Urbanc <urbancmarko1@gmail.com>
2024-07-28 16:43:07 +0200

    feat: :sparkles: working Ising model optimizer

--- IsingModel/Code/metropolistwo.py
+++ IsingModel/Code/metropolistwo.py
@@ -12,15 +12,79 @@ class Metropolis2:
                  # state_bounds: tuple,
                  quiet: bool = False,
                  ) -> None:
+        self.J = 1
+        self.H = 0
+        self.MAX_ITER = 1000000
+        self.STOP_STEPS = 500
+        self.EPS = 1e-8
+        self.quiet = quiet
+
         self.x_dim, self.y_dim = dimensions
         self.temperature = temperature
         # self.state_bounds = (state_bounds[0], state_bounds[1] + 1)
 
-        states = []
-        for i in range(10000):
-            self.state = np.random.choice([-1, 1], (self.x_dim, self.y_dim))
-            states.append(self.state)
+        self.state = np.random.choice([-1, 1], (self.x_dim, self.y_dim))
+        self.init_state = np.copy(self.state)
 
+    def _randomize_state(self) -> None:
+        self.state = np.random.choice([-1, 1], (self.x_dim, self.y_dim))
 
-        self.states = np.array(states).mean(axis=0)
+    def _energy(self) -> float:
+        energy = 0
+        for i in range(self.x_dim):
+            for j in range(self.y_dim):
+                energy += -self.J * self.state[i, j] * (self.state[(i+1) % self.x_dim, j] + self.state[i, (j+1) % self.y_dim])
+
+        if self.H:
+            energy += -self.H * np.sum(self.state)
+            
+        return energy
+    
+    def _delta_energy(self, i, j, prev_energy) -> float:
+        return 2 * self.J * self.state[i, j] * (self.state[(i-1) % self.x_dim, j] + 
+                                                self.state[(i+1) % self.x_dim, j] + 
+                                                self.state[i, (j-1) % self.y_dim] +
+                                                self.state[i, (j+1) % self.y_dim]) + 2 * self.H * self.state[i, j]
+    
+
+    def run(self):
+        self.energies = []
+        self.energies.append(self._energy())
+
+        iter = 0
+
+        # Main loop
+        while True:
+            # Choose a random location in the chain
+            i, j = np.random.randint(0, self.x_dim), np.random.randint(0, self.y_dim)
+            delta_energy = self._delta_energy(i, j, self.energies[-1])
+            
+            if delta_energy <= 0:
+                self.state[i, j] *= -1
+            else:
+                if np.random.rand() < np.exp(-delta_energy / self.temperature):
+                    self.state[i, j] *= -1
+
+            self.energies.append(delta_energy)
+            if iter > self.STOP_STEPS:
+                exit_cond = np.sum(np.abs(np.diff(self.energies[-1000:])))
+                if exit_cond < self.EPS:
+                       break
+                if not self.quiet:
+                    print(f"Step: {iter}, Exit condition: {exit_cond}, Delta Energy: {delta_energy}", end="\r")
+            
+            if iter > self.MAX_ITER:
+                break
+
+            iter += 1
+        
+        if not self.quiet:
+            print("\n")
+            print("Simulation complete.")
+            print(f"Total steps: {iter}")
+            print(f"Inital energy: {self.energies[0]}")
+            print(f"Final energy: {self.energies[-1]}")
+
+        return self.init_state, self.state, self.energies
+                
 
--- IsingModel/Code/test.py
+++ IsingModel/Code/test.py
@@ -6,9 +6,19 @@ import matplotlib.pyplot as plt
 from metropolistwo import Metropolis2
 
 m = Metropolis2((100, 100), 1)
+s_init, s_final, energies = m.run()
 
-fig, ax = plt.subplots()
-ax.imshow(m.states, cmap='gray')
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+
+ax[0].imshow(s_init, cmap='gray')
+ax[0].set_title("Initial state")
+ax[0].axis('off')
+
+ax[1].imshow(s_final, cmap='gray')
+ax[1].set_title("Final state")
+ax[1].axis('off')
 
 plt.tight_layout()
+plt.savefig("./IsingModel/Images/isingmodel.png", dpi=400)
 plt.show()
