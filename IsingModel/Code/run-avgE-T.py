70e0800130b740917e61f00502635da5344ce508
Marko Urbanc <urbancmarko1@gmail.com>
2024-07-30 17:43:35 +0200

    feat: :sparkles: try to improve thread safety for HDF5 access
    Probably unnecessary

--- IsingModel/Code/run-avgE-T.py
+++ IsingModel/Code/run-avgE-T.py
@@ -6,6 +6,7 @@ import numpy as np
 import concurrent.futures
 from metropolistwo import Metropolis2
 import h5py
+import multiprocessing
 
 class Worker:
     def __init__(self,
@@ -20,7 +21,7 @@ class Worker:
         m = Metropolis2(dim, self.T)
         # Contrary to the silly name I chose this is not a random state, but a fixed one
         # The fixed state was created randomly in the create-fixed-init.py script, hence the name
-        m.state = np.load("./IsingModel/Results/random-state.npy")
+        # m.state = np.load("./IsingModel/Results/random-state.npy")
         m.MAX_ITER = 1e5
         m.quiet = True
         print(f"[INFO] Starting: T={self.T}\tRun={self.run}")
@@ -32,12 +33,27 @@ class Worker:
 def worker_task(params):
     worker = Worker(*params)
     return worker.process()
+
+# Create function for thread-safe writing
+def write_data(save_path, key, s_final, en, temps, lock):
+    with lock:
+        with h5py.File(save_path, "a") as f:
+            group = f.create_group(f"{key}")
+            dset_state = group.create_dataset(f"state-{key}", data=s_final,
+                                            compression="gzip", compression_opts=9)
+            dset_en = group.create_dataset(f"energy-{key}", data=en, compression="gzip",
+                                        compression_opts=9)
+            dset_t = group.create_dataset(f"temperature-{key}", data=temps, compression="gzip",
+                                        compression_opts=9)
     
 # Get Process Pool parameter list
 runs = 100
 param_list = np.genfromtxt("./IsingModel/Code/T_range.lst")
 param_list = [[par, i] for par in param_list for i in range(runs)]
 
+# Create manager and lock for thread-safe writing
+manager = multiprocessing.Manager()
+lock = manager.Lock()
 
 # Run process pool and save on thread execution
 with concurrent.futures.ProcessPoolExecutor(max_workers=16) as executor:
@@ -50,11 +66,5 @@ with concurrent.futures.ProcessPoolExecutor(max_workers=16) as executor:
         arg2 = run
         save_path = "./IsingModel/Results/avgE-vs-T-fixedscale.h5"
         print(f"Storing run {arg1},{arg2} to '{save_path}'..")
-
-        with h5py.File(save_path, "a") as f:
-            key = f"{arg1}-{arg2}"
-            group = f.create_group(f"{key}")
-            dset_state = group.create_dataset(f"state-{key}", data=s_final)
-            dset_en = group.create_dataset(f"energy-{key}", data=en)
-            dset_t = group.create_dataset(f"temperature-{key}", data=temps)
+        write_data(save_path, f"{arg1}-{arg2}", s_final, en, temps, lock)
         print(f"Saved!")
