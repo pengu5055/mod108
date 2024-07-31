5b4d822657b4d16a74799c01a174119bc8638ad2
Marko Urbanc <urbancmarko1@gmail.com>
2024-07-31 23:55:08 +0200

    feat: :art: upload ultra long expanded plot

--- IsingModel/Code/check-hdf5.py
+++ IsingModel/Code/check-hdf5.py
@@ -7,8 +7,12 @@ import h5py
 
 
 # Load the file
-save_path = "./IsingModel/Results/H-analysis-noanneal.h5"
-with h5py.File(save_path, "r") as f:
-    key = list(f.keys())
-    print(len(key))
+save_path = "./IsingModel/Results/H-analysis-v2.h5"
+T_values = np.loadtxt("./IsingModel/Code/T_range.lst")
 
+with h5py.File(save_path, "r") as f:
+    for T in T_values:
+        key = f"{T}-0"
+        group = f[key]
+        
+ 
