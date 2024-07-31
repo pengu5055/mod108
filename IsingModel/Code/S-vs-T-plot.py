9a6aced444cf5b0b32a4c7aa996fc9f383488d3f
Marko Urbanc <urbancmarko1@gmail.com>
2024-07-31 15:12:40 +0200

    feat: :art: improve main Ising plot

---
+++ IsingModel/Code/S-vs-T-plot.py
@@ -0,0 +1,134 @@
+"""
+Plot the average relaxation energy of the system as a function of temperature.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import h5py
+import cmasher as cmr
+import palettable as pl
+
+# Enable custom stylesheet
+plt.style.use('./ma-style.mplstyle')
+mpl.use("qtagg")
+
+# Simulation parameters
+save_path = "./IsingModel/Results/avgE-vs-T-fixedscale.h5"
+bounds = (-18, 0)
+length = 17
+temperature = 1
+init = [0, -17, -4, -2, -8, -13, -7, -2, -5, -2, -3, -2, -3, -10, -15, -7, 0]
+molecules = np.arange(0, 17)
+T_values = np.loadtxt("./IsingModel/Code/T_range.lst")
+
+# Containers for data
+steps_to_convergence = []
+end_energy = []
+end_temperature = []
+end_states = []
+# Load data
+with h5py.File(save_path, "r") as f:
+    num_runs = 100
+
+    for i, T in enumerate(T_values):
+        row_states = []
+        row_steps = []
+        row_end_energy = []
+        row_end_temperature = []
+
+        for run in range(num_runs):
+            key = f"{T}-{run}"
+            group = f[key]
+            row_states.append(group[f"state-{key}"][:])
+            row_steps.append(len(group[f"energy-{key}"]))
+            row_end_energy.append(group[f"energy-{key}"][-1])
+            row_end_temperature.append(group[f"temperature-{key}"][-1])
+
+        end_states.append(row_states)
+        steps_to_convergence.append(row_steps)  
+        end_energy.append(row_end_energy)
+        end_temperature.append(row_end_temperature)
+
+# Calculate statistics
+steps_to_convergence = np.array(steps_to_convergence)
+avg_steps = np.mean(steps_to_convergence, axis=1)
+sigma_steps = np.std(steps_to_convergence - avg_steps, axis=1)
+
+end_energy = np.array(end_energy)
+avg_energy = np.mean(end_energy, axis=1)
+sigma_energy = np.std(end_energy, axis=1)
+
+end_temperature = np.array(end_temperature)
+avg_temperature = np.mean(end_temperature, axis=1)
+delta_temp = T_values - avg_temperature
+sigma_temperature = np.std(T_values - end_temperature, axis=1)
+
+# Calculate Observables: Eigenmagnetization
+def magnetization(state: np.ndarray):
+    S = np.array([[np.sum(np.array(final[run])) for run in range(num_runs)] for final in end_states]) 
+    return S
+
+S = np.abs(magnetization(end_states))
+avgS = np.mean(S, axis=1)
+sigmaS = np.std(S, axis=1)
+
+# Plot data as 3 subplots
+colors = ["#37123c","#d72483","#ddc4dd","#60afff","#98CE00"]
+
+fig, ax = plt.subplots(2, 2, figsize=(12, 9))
+
+# Average energy vs. temperature
+ax[0, 0].plot(T_values, avg_energy, color=colors[1], alpha=1, lw=3, label="Average Energy")
+ax[0, 0].plot(T_values, avg_energy - sigma_energy, color=colors[0], alpha=1, ls="--")
+ax[0, 0].plot(T_values, avg_energy + sigma_energy, color=colors[0], alpha=1, ls="--")
+ax[0, 0].fill_between(T_values, avg_energy - sigma_energy, avg_energy + sigma_energy, 
+                      color=colors[3], alpha=0.3, label="1-$\sigma$ Band")
+
+ax[0, 0].legend(frameon=False)
+ax[0, 0].set_ylabel("Average Energy [arb. units]")
+ax[0, 0].set_xlabel("Temperature")
+ax[0, 0].set_title(r"Avg. Final Energy Over $100$ Runs")
+
+
+# Average magnetization vs. temperature
+ax[0, 1].plot(T_values, avgS, color=colors[1], alpha=1, lw=3, label="Average Magnetization")
+ax[0, 1].plot(T_values, avgS - sigmaS, color=colors[0], alpha=1, ls="--")
+ax[0, 1].plot(T_values, avgS + sigmaS, color=colors[0], alpha=1, ls="--")
+ax[0, 1].fill_between(T_values, avgS - sigmaS, avgS + sigmaS, 
+                      color=colors[3], alpha=0.3, label="1-$\sigma$ Band")
+
+ax[0, 1].legend(frameon=False)
+ax[0, 1].set_ylabel("|Average Magnetization| [arb. units]")
+ax[0, 1].set_xlabel("Temperature")
+ax[0, 1].set_title(r"|Avg. Magnetization| Over $100$ Runs")
+
+# End Energy vs. Temperature vs. Run
+cm = cmr.get_sub_cmap('cmr.bubblegum', 0., 1.)
+norm = mpl.colors.Normalize(vmin=end_energy.min(), vmax=end_energy.max())
+sm = plt.cm.ScalarMappable(cmap=cm, norm=norm)
+ax[1, 0].imshow(end_energy.T, aspect="auto", cmap=cm, norm=norm,
+                extent=[T_values[0], T_values[-1], 100, 1])
+cbar = plt.colorbar(sm, ax=ax[1, 0])
+cbar.set_label("Energy [arb. units]")
+ax[1, 0].set_ylabel("Run")
+ax[1, 0].set_xlabel("Temperature")
+ax[1, 0].set_title("Final Energy Spread Across Runs")  
+
+# Steps to Convergence vs. Temperature vs. Run
+cm = cmr.get_sub_cmap('cmr.bubblegum', 0., 1.)
+norm = mpl.colors.Normalize(vmin=S.min(), vmax=S.max())
+sm = plt.cm.ScalarMappable(cmap=cm, norm=norm)
+
+ax[1, 1].imshow(S.T, aspect="auto", cmap=cm, norm=norm,
+                extent=[T_values[0], T_values[-1], 1, 100])
+cbar = plt.colorbar(sm, ax=ax[1, 1])
+cbar.set_label("|Magnetization| [arb. units]")
+# cbar.set_ticks([steps_to_convergence.min(), 1000, 10000, steps_to_convergence.max()])
+# cbar.set_ticklabels([steps_to_convergence.min(), "1000", "10000", steps_to_convergence.max()])
+ax[1, 1].set_ylabel("Run")
+ax[1, 1].set_xlabel("Temperature")
+ax[1, 1].set_title("|Magnetizaion| Spread Across Runs")  
+
+plt.tight_layout()
+plt.savefig("./IsingModel/Images/avgE-avgS-vs-T.png", dpi=500)
+plt.show()
