c756712ef1a5ade297efa8c8cceaea02dad59254
Marko Urbanc <urbancmarko1@gmail.com>
2024-07-31 23:49:25 +0200

    feat: :art: add multiple expanded plots

--- IsingModel/Code/check-hdf5.py
+++ IsingModel/Code/check-hdf5.py
@@ -5,27 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 
-init_state = np.load("./IsingModel/Results/random-state.npy")
 
 # Load the file
-save_path = "./IsingModel/Results/avgE-vs-T-pool.h5"
+save_path = "./IsingModel/Results/H-analysis-noanneal.h5"
 with h5py.File(save_path, "r") as f:
-    key = list(f.keys())[-1]
-    state = f[key][f"state-{key}"][:]
-
-
-# Debug plots
-fig, ax = plt.subplots(1, 2, figsize=(12, 5))
-
-ax[0].imshow(init_state, cmap="gray")
-ax[0].set_title("Initial state")
-
-ax[1].imshow(state, cmap="gray")
-ax[1].set_title("Final state")
-
-plt.suptitle(f"Init. vs Final for T={float(key):.2e}")
-
-plt.show()
-
-
+    key = list(f.keys())
+    print(len(key))
 
